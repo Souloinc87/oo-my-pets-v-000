88748376d526b14af4da586765ec592843104584
Marcus  = Soulo <souloinc87@gmail.com>
2018-04-21 05:19:10 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog
-    attr_reader :name
-    attr_accessor :mood, :owner
+  attr_reader :name
+  attr_accessor :mood, :owner
 
-    def initialize(name)
-      @name = name
-      @mood = "nervous"
-    end
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
 end
