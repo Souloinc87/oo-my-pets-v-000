7b0e89680563b9d1b3a44d79a2c768fe1e10b3b5
Marcus  = Soulo <souloinc87@gmail.com>
2018-04-21 05:00:12 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,85 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  attr_accessor :owner, :name, :pets, :fish, :dog, :cat, :mood
+  attr_reader :species
+
+  @@all = []
+
+  @@owner = 0
+
+  def initialize(species)
+    @owner = owner
+    @species = species
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+    @@all << self
+    @@owner += 1
+    @mood = mood
+  end
+
+  def self.count
+    self.all.count
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def self.all
+    @@all
+  end
+
+  def say_species
+     "I am a #{@species}."
+  end
+
+  def buy_fish(fish)
+    fish = Fish.new(fish)
+    @pets[:fishes] << fish
+  end
+
+  def buy_cat(cat)
+    cat = Cat.new(cat)
+    @pets[:cats] << cat
+  end
+
+  def buy_dog(dog)
+    dog = Dog.new(dog)
+    @pets[:dogs] << dog
+  end
+
+  def walk_dogs
+    self.pets[:dogs].map do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def play_with_cats
+    cat = Cat.new(cat)
+    self.pets[:cats].map do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    fish = Fish.new(fish)
+    self.pets[:fishes].map do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    binding.pry 
+    self.pets[:fishes].map {|x| x.mood = "nervous"}
+    self.pets[:cats].map {|x| x.mood = "nervous"}
+    self.pets[:dogs].map {|x| x.mood = "nervous"}
+
+    self.pets.clear
+  end
+
+  def list_pets
+    #binding.pry
+    "I have #@pets[:fishes][0].count fish, #@pets[:dogs].count dog(s), and #@pets[:cat].count cat(s)."
+  end
+
 end
